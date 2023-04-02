1a377138d50f7505f701d8b4379d04307c8c5522
joshuamd21 <joshua.downey21@gmail.com>
2023-04-01 21:31:12 -0500

    Added Bounds and player rotation
    Added a boundary to the map in the form of a square areana that blocks the player in. Still need to setup the arena to stop the player as player can currently fly through the box.
    
    Added player rotation so that as they move they rotate towards the direction in which they are moving.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private float health;
     private float damage;
     private float speed = 20;
+    private float rotationSpeed = 360;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +17,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        movePlayer();
+    }
+
+    private void movePlayer()
+    {
         float horiz = Input.GetAxis("Horizontal");
         float vertic = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horiz);
-        transform.Translate(Vector3.up * Time.deltaTime * speed * vertic);
+
+        Vector2 movementDirection = new Vector2(horiz, vertic);
+
+        transform.Translate(movementDirection * Time.deltaTime * speed, Space.World);
+
+        if (movementDirection != Vector2.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+
     }
 }
