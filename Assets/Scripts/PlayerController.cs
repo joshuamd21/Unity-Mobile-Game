062c5a530bca8460c7299b8df4ef140add3f4c49
joshuamd21 <joshua.downey21@gmail.com>
2023-04-01 21:40:37 -0500

    Fixed wall collision with player
    The walls now actually stop the player completely from going through the wall. This is because the player actually now uses Unity physics rather than just changing the position of the object.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     private float health;
     private float damage;
+    [SerializeField]
     private float speed = 20;
+    private float maginitude = 100;
     private float rotationSpeed = 360;
+    private Rigidbody2D rB;
     // Start is called before the first frame update
     void Start()
     {
-
+        rB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -27,7 +30,7 @@ public class PlayerController : MonoBehaviour
 
         Vector2 movementDirection = new Vector2(horiz, vertic);
 
-        transform.Translate(movementDirection * Time.deltaTime * speed, Space.World);
+        rB.AddForce(movementDirection * Time.deltaTime * maginitude * speed, ForceMode2D.Impulse);
 
         if (movementDirection != Vector2.zero)
         {
