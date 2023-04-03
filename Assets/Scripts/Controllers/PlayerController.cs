afcd4c21a689447aa3eb9a031eaefcf3578288c4
joshuamd21 <joshua.downey21@gmail.com>
2023-04-02 19:56:44 -0500

    Implemented basic player attack
    Implemented the ability for the player to shoot a projectile when clicking down.
    
    Projectile still does not do damage or hurt enemies
    
    the projectile keeps going until it hits something then sets itself back to false

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -4,26 +4,60 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private List<GameObject> bullets;
+    private GameObject bulletHolder;
+    private int maxBullets = 20;
     private float health;
     private float damage;
-    [SerializeField]
-    private float speed = 20;
+    private float speed = 10;
     private float maginitude = 100;
     private float rotationSpeed = 360;
+    [SerializeField]
+    private float radius;
     private Rigidbody2D rB;
+    public GameObject bulletPooled;
+
     // Start is called before the first frame update
     void Start()
     {
         rB = GetComponent<Rigidbody2D>();
+        bulletHolder = GameObject.Find("BulletHolder");
+        // Loop through list of pooled objects,deactivating them and adding them to the list 
+        bullets = new List<GameObject>();
+        for (int i = 0; i < maxBullets; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(bulletPooled);
+            obj.SetActive(false);
+            bullets.Add(obj);
+            obj.transform.SetParent(bulletHolder.transform);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        movePlayer();
+        MovePlayer();
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireBullet();
+        }
+    }
+    public GameObject GetPooledObject()
+    {
+        // For as many objects as are in the pooledObjects list
+        for (int i = 0; i < bullets.Count; i++)
+        {
+            // if the pooled objects is NOT active, return that object 
+            if (!bullets[i].activeInHierarchy)
+            {
+                return bullets[i];
+            }
+        }
+        // otherwise, return null   
+        return null;
     }
 
-    private void movePlayer()
+    private void MovePlayer()
     {
         float horiz = Input.GetAxis("Horizontal");
         float vertic = Input.GetAxis("Vertical");
@@ -38,5 +72,19 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
 
+    }
+    private void FireBullet()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+        worldPosition.z = 0;
+        Vector3 toMouse = (worldPosition - transform.position).normalized;
+        GameObject bullet = GetPooledObject();
+        if (bullet is not null)
+        {
+            bullet.SetActive(true);
+            bullet.transform.right = toMouse;
+            bullet.transform.position = transform.position + toMouse * radius;
+        }
     }
 }
